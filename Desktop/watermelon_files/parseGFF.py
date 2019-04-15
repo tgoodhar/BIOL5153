adeec29d8417370e039e6ad29137d5bd0cf1c89b
Timothy Goodhart <timothygoodhart@tim-2.ddns.uark.edu>
2019-04-15 07:56:29 -0500

    use csv reader to parse Gff file and use seqIO to parse the fasta file genome

--- Desktop/watermelon_files/parseGFF.py
+++ Desktop/watermelon_files/parseGFF.py
@@ -5,3 +5,27 @@ for seq_record in SeqIO.parse("watermelon.fsa", "fasta"):
     print(seq_record.id)
     print(repr(seq_record.seq))
     print(len(seq_record))
+
+
+
+import csv
+
+#make an output file. Needed when writing out file (create csv writer object).
+outfile = 'watermelon.tab'
+
+#open the output file.  "tabout is the file handle".
+with open(outfile, 'w') as tabout:
+
+# open the data file
+    with open('watermelon.csv', 'r') as watermelon:
+
+        # create a csv reader object
+        csvreader = csv.reader(shaver, delimiter=',')
+
+        for line in csvreader:
+            if not line:
+                continue
+            else:
+                # create csv writer object
+                linewriter = csv.writer(tabout, delimiter='\t', quotechar='"')
+                linewriter.writerow(line)
