4469e2dfb25ef653b1bc3e71c3268ed74f0d2d58
Timothy Goodhart <timothygoodhart@Tim-2.local>
2019-04-22 06:09:28 -0500

    assn_07

--- Desktop/watermelon_files/parseGFF.py
+++ Desktop/watermelon_files/parseGFF.py
@@ -1,31 +1,76 @@
 #! /usr/bin/env python3
 
-from Bio import SeqIO
-for seq_record in SeqIO.parse("watermelon.fsa", "fasta"):
-    print(seq_record.id)
-    print(repr(seq_record.seq))
-    print(len(seq_record))
+import argparse
+
+parser = argparse.ArgumentParser(description = "")
+
+# add positional arguments, i.e., the required input
+parser.add_argument( "-gf", "--gff", help="the name of the gff file", default="watermelon.gff")
+# when running the script, ./parseGff.py -gf watermelon.gff
+parser.add_argument( "-fa", "--fasta", help="the name of the fasta file", default="watermelon.fsa")
+# when running the script, ./parseGFF.py -fa watermelon.fsa
+
+# parse the command line arguments
+args = parser.parse_args()
+
+# read gff file
+gff_file = args.gff
+gff = open(gff_file, "r")
+gff_output = open("gff_output.txt", "w")
+
+for line in gff:
+    fields = line.split('\t')
+    start = int(fields[3])
+    stop = int(fields[4])
+    print(str(start) + "\t" + str(stop))
+    
+    gff_substring = (fields[3], fields[4])
+    
+    gff_output.write(str(start) + "\t" + str(stop) + "\n")
+
+# read fasta file
+fasta_file = open(args.fasta).read()
+fasta_output = open("gff_output.txt")
+substring = open("substring.txt", "w")
+
+for lines in fasta_output:
+    positions = lines.split('\t')
+    start = int(positions[0])
+    stop = int(positions[1])
+    substring_sequence = fasta[start:stop]
+    print(substring_sequence)
+
+# calculate GC content of each feature
+genome = open("substring.txt").read()
+length = len(genome)
+G_content = genome.count('G')
+C_content = genome.count('C')
+
+print("length: " + str(length))
+print("g count: " + str(G_content))
+print("C count: " + str(C_content))
+
+GC_content = (G_content + C_content) / length 
+
+print("GC content: " + str(gc_content))
+
+
+# calculating complements
+replacement1 = genome.replace('A', 't')
+print(replacement1)
+replacement2 = replacement1.replace('T', 'a')
+print(replacement2)
+replacement3 = replacement2.replace('C', 'g')
+print(replacement3)
+replacement4 = replacement3.replace('G', 'c')
+print(replacement4)
+
+print("sequence complement: " + replacement4.upper())
 
 
 
-import csv
-
-#make an output file. Needed when writing out file (create csv writer object).
-outfile = 'watermelon.tab'
-
-#open the output file.  "tabout is the file handle".
-with open(outfile, 'w') as tabout:
-
-# open the data file
-    with open('watermelon.csv', 'r') as watermelon:
-
-        # create a csv reader object
-        csvreader = csv.reader(shaver, delimiter=',')
-
-        for line in csvreader:
-            if not line:
-                continue
-            else:
-                # create csv writer object
-                linewriter = csv.writer(tabout, delimiter='\t', quotechar='"')
-                linewriter.writerow(line)
+#from Bio import SeqIO
+#genome = SeqIO.read('watermelon.fsa', 'fasta')
+#print(genome.seq)
+#print(len(genome.seq))
+#print(genome.reverse_complement)
